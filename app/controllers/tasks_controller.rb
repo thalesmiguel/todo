71b126195a5e64ef5ea231dcb7f19f58f3294585
thalesmiguel <thales.miguel@gmail.com>
2016-03-11 18:16:12 -0300

    First Commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,28 +4,23 @@ class TasksController < ApplicationController
   after_filter :prepare_unobtrusive_flash
 
 
-  # GET /tasks
-  # GET /tasks.json
   def index
-    @tasks = Task.all
+    myTasks = Task.where(:user_id => current_user.id)
+    publicTasks = Task.where(:public => true)
+    
+    @tasks = myTasks + publicTasks
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
   def show
   end
 
-  # GET /tasks/new
   def new
     @task = Task.new
   end
 
-  # GET /tasks/1/edit
   def edit
   end
 
-  # POST /tasks
-  # POST /tasks.json
   def create
     @task = Task.new(task_params)
     @task.user_id = current_user.id
@@ -41,8 +36,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
   def update
     respond_to do |format|
       if @task.update(task_params)
@@ -55,8 +48,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # DELETE /tasks/1
-  # DELETE /tasks/1.json
   def destroy
     @task.destroy
     respond_to do |format|
@@ -67,12 +58,10 @@ class TasksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_task
       @task = Task.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
       params.require(:task).permit(:description, :public, :user_id)
     end
